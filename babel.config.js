fbf4f0802498a18268032d59885caf7f180deea3
Justin Harris <api@natively.dev>
2025-10-30 12:45:34 +0000

    fix: Add unique names to worklets and reanimated Babel plugins

--- babel.config.js
+++ babel.config.js
@@ -45,9 +45,10 @@ module.exports = function (api) {
         "react-native-worklets/plugin",
         {
           processNestedWorklets: true,
-        }
+        },
+        "react-native-worklets-plugin"
       ],
-      "react-native-reanimated/plugin", // Must be last
+      ["react-native-reanimated/plugin", {}, "react-native-reanimated-plugin"], // Must be last
     ],
   };
 };
